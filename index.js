ea0672edfa6ff1f5ae42900eb330701ae7c27c03
DXTECH <darkxtechzone@gmail.com>
2026-09-05 10:48:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const { startSock, getPairingCodeForNumber, getStatus } = require('./lib/whatsapp');
